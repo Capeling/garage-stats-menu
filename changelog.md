80ed83816378bb0026c66259d59b3a5da82917d5
Capeling <113705504+Capeling@users.noreply.github.com>
2024-06-23 18:02:40 +0930

    1.0.1

--- changelog.md
+++ changelog.md
@@ -1,3 +1,5 @@
 # Stats Display API Changelog
+## v1.0.1
+- Fix for other platforms (Hopefully)
 ## v1.0.0
 - Initial release
--- include/StatsDisplayAPI.h
+++ include/StatsDisplayAPI.h
@@ -3,7 +3,7 @@
 #include <Geode/Geode.hpp>
 
 #ifdef GEODE_IS_WINDOWS
-	#ifdef CAPELING_STATS_API_EXPORTING
+	#ifdef CAPELING_STATSAPI_EXPORTING
 		#define STATS_API_DLL __declspec(dllexport)
 	#else
 		#define STATS_API_DLL __declspec(dllimport)
@@ -12,8 +12,26 @@
 		#define STATS_API_DLL
 #endif
 
-class STATS_API_DLL StatsDisplayAPI {
-public:
-	static cocos2d::CCNode* getNewItem(std::string ID, cocos2d::CCNode* displayNode, int displayedNum, float nodeScale = 0.5f);
+namespace StatsDisplayAPI {
+	cocos2d::CCNode* getNewItem(std::string ID, cocos2d::CCNode* displayNode, int displayedNum, float nodeScale = 0.5f) {
+		auto ret = CCMenu::create();
 
+		displayNode->setID(ID + "-icon");
+		displayNode->setScale(nodeScale);
+		displayNode->removeFromParentAndCleanup(false);
+		displayNode->setPosition({ 0, 0 });
+		ret->addChild(displayNode);
+
+		auto label = CCLabelBMFont::create(std::to_string(displayedNum).c_str(), "bigFont.fnt");
+		label->setID(ID + "-label");
+		label->setScale({ 0.34 });
+		label->setAnchorPoint({ 1, 0.5 });
+		label->removeFromParentAndCleanup(false);
+		label->setPosition({ -12, 0.5 });
+		ret->addChild(label);
+
+		ret->setID(ID + "-container");
+		ret->setContentSize({ 0, 0 });
+		return ret;
+	}
 };
